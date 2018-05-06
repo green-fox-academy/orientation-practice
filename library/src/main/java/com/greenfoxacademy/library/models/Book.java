614383498b900a4ea0f346716c53925f8712ff15
Mark Kovari <mark@Marks-macbook.local>
2018-05-06 10:51:23 +0200

    add(models): Book, User

---
+++ library/src/main/java/com/greenfoxacademy/library/models/Book.java
@@ -0,0 +1,72 @@
+package com.greenfoxacademy.library.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "book_id")
+    private Integer id;
+
+    private String title;
+    private String author;
+    @Column(name = "isbn")
+    private String ISBN;
+
+    @OneToOne(mappedBy = "user_id")
+    private List<User> borrowedBy;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String ISBN, List<User> borrowedBy) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.borrowedBy = borrowedBy;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public List<User> getBorrowedBy() {
+        return borrowedBy;
+    }
+
+    public void setBorrowedBy(List<User> borrowedBy) {
+        this.borrowedBy = borrowedBy;
+    }
+}
---
+++ library/src/main/java/com/greenfoxacademy/library/models/User.java
@@ -0,0 +1,52 @@
+package com.greenfoxacademy.library.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "user_id")
+
+    private Integer id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "book_id")
+    private List<Book> borrowedBooks;
+
+    public User() {
+    }
+
+    public User(String name, List<Book> borrowedBooks) {
+        this.name = name;
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+}
